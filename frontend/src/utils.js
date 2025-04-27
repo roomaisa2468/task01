c4c77797f6021e63dbbcb7df65bd3aa977559841
Rumaisa Ghayas Uddin <varishauddin763@gmail.com>
2025-04-27 21:42:11 +0500

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,7 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-// export const API_URL = 'https://mern-task-manager-app-deploy-api.vercel.app';
-// utils.js
-export const API_URL = "https://task01-rose.vercel.app"; // Ensure this points to your backend's URL
-
+export const API_URL = 'https://mern-task-manager-app-deploy-api.vercel.app';
