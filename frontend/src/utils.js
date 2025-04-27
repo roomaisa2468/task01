fa0e99b762124e3bd645cb986bd500e407a6f60f
Rumaisa Ghayas Uddin <varishauddin763@gmail.com>
2025-04-27 22:03:38 +0500

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'https://mern-task-manager-app-deploy-api.vercel.app';
+export const API_URL = 'https://task01-ui-rumaisas-projects-78495398.vercel.app/';
